df344bfae96418f215e7ef47e042cbfbf8bb33aa
ddejrufit <rkd555777@naver.com>
2025-04-14 21:00:52 +0900

    부트스트랩 네브바

---
+++ src/components/Navigation.js
@@ -0,0 +1,19 @@
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function Navigation() {         
+    return(
+      <Navbar bg="light" data-bs-theme="light">
+      <Container>
+        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+        <Nav className="me-auto">
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#features">Features</Nav.Link>
+          <Nav.Link href="#pricing">Pricing</Nav.Link>
+        </Nav>
+      </Container>
+    </Navbar>
+    );
+}
+
+export default Navigation;
