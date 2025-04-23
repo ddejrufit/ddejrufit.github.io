47859a17ac7fef2e1d996153f0fe66826eefed4b
ddejrufit <rkd555777@naver.com>
2025-04-23 23:45:14 +0900

    4/23

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -10,8 +10,8 @@ function Navigation() {
         <Navbar.Brand href="#home" >Navbar</Navbar.Brand>
         <Nav className="me-auto">
           <Nav.Link href="#home">   Home</Nav.Link>
-          <Nav.Link href="#features">   Features</Nav.Link>
-          <Nav.Link href="#pricing">   Pricing</Nav.Link>
+          <Nav.Link href="#About">   About</Nav.Link>
+          <Nav.Link href="#Project">   Project</Nav.Link>
         </Nav>
       </Container>
     </Navbar>
