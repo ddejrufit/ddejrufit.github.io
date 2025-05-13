d236e1af08a8601442358c7d08e6e2adfa8598ea
ddejrufit <rkd555777@naver.com>
2025-05-14 00:09:38 +0900

    5/14

--- src/data/projects.js
+++ src/data/projects.js
@@ -2,23 +2,22 @@
 const projects = [
     {
       id: 1,
-      title: "프로젝트 1: 멋진 웹사이트",
-      description: "사용자들이 편리하게 정보를 얻을 수 있는 반응형 웹사이트입니다.",
+      title: "블로그",
+      description: "react를 이용한 블로그",
       imageUrl: "https://i.ibb.co/프로젝트1_썸네일_이미지.png", // 프로젝트 썸네일 이미지 (선택 사항)
-      techStacks: ["React", "Node.js", "MongoDB", "CSS"], // 사용 기술 스택 목록
-      githubUrl: "https://github.com/ddejrufit/project-repo-1", // GitHub 리포지토리 링크
-      deployUrl: "https://project-1.netlify.app", // 배포된 웹사이트 링크 (선택 사항)
+      techStacks: ["React", "JavaScript", "HTML", "CSS"], // 사용 기술 스택 목록
+      githubUrl: "https://github.com/ddejrufit/reactblog", // GitHub 리포지토리 링크
+      deployUrl: "https://ddejrufit.github.io/reactblog/", // 배포된 웹사이트 링크 (선택 사항)
     },
     {
       id: 2,
-      title: "프로젝트 2: 모바일 앱",
-      description: "React Native를 활용한 간단한 모바일 애플리케이션입니다.",
+      title: "쇼핑몰",
+      description: "react를 이용한 쇼핑몰",
       imageUrl: "https://i.ibb.co/프로젝트2_썸네일_이미지.png",
-      techStacks: ["React Native", "Firebase"],
-      githubUrl: "https://github.com/ddejrufit/project-repo-2",
-      deployUrl: null, // 배포 링크가 없다면 null
+      techStacks: ["React","JavaScript","HTML","CSS"],
+      githubUrl: "https://github.com/ddejrufit/reactshop",
+      deployUrl: "https://ddejrufit.github.io/reactshop/",
     },
-    // ... 다른 프로젝트 정보들 추가 ...
   ];
   
   export default projects;
