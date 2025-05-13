2c4afdfd8ec62277fcda62f9f4090cb8dc52cfa5
ddejrufit <rkd555777@naver.com>
2025-05-14 00:41:21 +0900

    5/14

--- src/data/projects.js
+++ src/data/projects.js
@@ -4,7 +4,7 @@ const projects = [
       id: 1,
       title: "블로그",
       description: "react를 이용한 블로그",
-      imageUrl: "https://i.ibb.co/프로젝트1_썸네일_이미지.png", // 프로젝트 썸네일 이미지 (선택 사항)
+      imageUrl: "https://i.ibb.co/JjTfV7sD/reactblog.png", // 프로젝트 썸네일 이미지 (선택 사항)
       techStacks: ["React", "JavaScript", "HTML", "CSS"], // 사용 기술 스택 목록
       githubUrl: "https://github.com/ddejrufit/reactblog", // GitHub 리포지토리 링크
       deployUrl: "https://ddejrufit.github.io/reactblog/", // 배포된 웹사이트 링크 (선택 사항)
